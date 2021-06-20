afb1da196750be4942f75e9f9bfd815c4ffdfcaf
Viktor590 <resident1081994@gmail.com>
2021-06-20 19:00:50 +0300

    added section shop

---
+++ app/js/main.js
@@ -0,0 +1,5 @@
+const mixerProducts = mixitup('.shop-filter__item-wrapper', {
+  selectors: {
+    control: '.btn'
+  }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,8 @@ function styles() {
 function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/swiper/slick/slick.js',
         'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
